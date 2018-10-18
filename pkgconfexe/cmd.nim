acdd8e199674fe035f11228d4f1138e9fb4819cd
Thanh Tung Nguyen <39303840+thenjip@users.noreply.github.com>
2018-10-18 23:27:50 +0200

    Use a const to find out what goes wrong.

--- pkgconfexe/cmd.nim
+++ pkgconfexe/cmd.nim
@@ -23,33 +23,31 @@ type
 
 
 
-func buildCmdLine* (a: Action; me: ModuleEnv): string {. locks: 0 .} =
-  let
-    cmd =
-      if me.m.version.len() == 0:
-        fmt"""{CmdName} {$a} "{me.m.pkg}{'"'}"""
-      else:
-        fmt(
-          "{CmdName} {$a} " &
-            """{me.m.op.option()} "{me.m.version}" "{me.m.pkg}{'"'}"""
-        )
-    env = me.envVars.buildEnv()
+func buildCmdLine* (me: ModuleEnv; a: Action): string {. locks: 0 .} =
+  const cmd =
+    if me.m.version.len() == 0:
+      fmt"""{CmdName} {$a} "{me.m.pkg}{'"'}"""
+    else:
+      fmt(
+        "{CmdName} {$a} " &
+          """{me.m.op.option()} "{me.m.version}" "{me.m.pkg}{'"'}"""
+      )
 
   result =
     if me.envVars.len() == 0:
       cmd
     else:
-      fmt"{env} {cmd}"
+      fmt"{me.envVars.buildEnv()} {cmd}"
 
 
 
-func execCmds (action: Action; modEnvs: openarray[ModuleEnv]): string {.
+func execCmds (modEnvs: openarray[ModuleEnv]; action: Action): string {.
   compileTime, locks: 0
 .} =
   var results = newSeqofCap[string](modEnvs.len())
 
   for me in modEnvs:
-    results.add(staticExec(action.buildCmdLine(me)))
+    results.add(staticExec(me.buildCmdLine(action)))
 
   result = results.join($' ')
 
@@ -57,13 +55,13 @@ func execCmds (action: Action; modEnvs: openarray[ModuleEnv]): string {.
 func getCFlags* (modEnvs: openarray[ModuleEnv]): string {.
   compileTime, locks: 0
 .} =
-  result = execCmds(Action.CFlags, modEnvs)
+  result = modEnvs.execCmds(Action.CFlags)
 
 
 func getLdFlags* (modEnvs: openarray[ModuleEnv]): string {.
   compileTime, locks: 0
 .} =
-  result = execCmds(Action.LdFlags, modEnvs)
+  result = modEnvs.execCmds(Action.LdFlags)
 
 
 
